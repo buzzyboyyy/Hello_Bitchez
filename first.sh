36fce4e1f3628f7631e499f414ad34769878d313
buzzyboyyy <lilcvbballfreak@gmail.com>
2022-07-20 01:13:03 +0000

    first commit

--- first.sh
+++ first.sh
@@ -3,3 +3,4 @@ read name
 echo "I've shit bigger turds than you, $name. How'd you do in there at the 2/5 poker table?"
 read remark
 echo "Been there done that, I've $remark 100s of times before you were even born bucko."
+# Hello_Bitchez
